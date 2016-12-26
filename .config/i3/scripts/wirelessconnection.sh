db822bd4d45ed7339533ff7721dd3d9094729115
Lance Lafontaine <lance.lafontaine92@gmail.com>
2016-12-26 17:33:27 -0500

    Adds link quality indicator when connected to wireless

--- .config/i3/scripts/wirelessconnection.sh
+++ .config/i3/scripts/wirelessconnection.sh
@@ -3,11 +3,12 @@ WIFIICON=""
 DISCONNECTEDICON=""
 ISCONNECTED="$(nmcli -terse -colors no -fields STATE general)"
 CONNECTIONNAME="$(nmcli -terse -fields NAME connection show --active)"
+LINKQUALITY="$(iwconfig | egrep -o '[0-9]*/70')"
 
 if [ ! "$ISCONNECTED" == "connected" ]; then
   echo "$DISCONNECTEDICON"
 else
-  echo "$WIFIICON $CONNECTIONNAME"
+  echo "$WIFIICON $CONNECTIONNAME $LINKQUALITY"
 fi
 
 
